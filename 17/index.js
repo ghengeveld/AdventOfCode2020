9737b43fe0a84e341fa69b5e1d12a0bc0bfec319
Gert Hengeveld <info@ghengeveld.nl>
2020-12-18 16:06:03 +0100

    Day 17 part 2

--- 17/index.js
+++ 17/index.js
@@ -16,26 +16,31 @@ const print = (items, neighbors = false) => {
   const rx = [Infinity, -Infinity]
   const ry = [Infinity, -Infinity]
   const rz = [Infinity, -Infinity]
-  for (const { x, y, z } of items) {
+  const rw = [Infinity, -Infinity]
+  for (const { x, y, z, w } of items) {
     if (x < rx[0]) rx[0] = x
     if (x > rx[1]) rx[1] = x
     if (y < ry[0]) ry[0] = y
     if (y > ry[1]) ry[1] = y
     if (z < rz[0]) rz[0] = z
     if (z > rz[1]) rz[1] = z
+    if (w < rw[0]) rw[0] = w
+    if (w > rw[1]) rw[1] = w
   }
-  for (let z = rz[0]; z <= rz[1]; z++) {
-    console.log(`z=${z}`)
-    for (let y = ry[0]; y <= ry[1]; y++) {
-      let line = ""
-      for (let x = rx[0]; x <= rx[1]; x++) {
-        const v = items.find((o) => o.x === x && o.y === y && o.z === z)
-        if (neighbors) line += v ? `${v.n}` : "0"
-        else line += v ? "#" : "."
+  for (let w = rw[0]; w <= rw[1]; w++) {
+    for (let z = rz[0]; z <= rz[1]; z++) {
+      console.log(`z=${z}, w=${w}`)
+      for (let y = ry[0]; y <= ry[1]; y++) {
+        let line = ""
+        for (let x = rx[0]; x <= rx[1]; x++) {
+          const v = items.find((o) => o.x === x && o.y === y && o.z === z && o.w === w)
+          if (neighbors) line += v ? `${v.n}` : "0"
+          else line += v ? "#" : "."
+        }
+        console.log(line)
       }
-      console.log(line)
+      console.log("")
     }
-    console.log("")
   }
 }
 
@@ -45,7 +50,9 @@ let active = input
   .reduce(
     (acc, line, y) =>
       acc.concat(
-        line.split("").reduce((a, c, x) => (c === "#" ? a.concat({ x, y, z: 0, n: 0 }) : a), [])
+        line
+          .split("")
+          .reduce((a, c, x) => (c === "#" ? a.concat({ x, y, z: 0, w: 0, n: 0 }) : a), [])
       ),
     []
   )
@@ -58,13 +65,15 @@ for (let i = 0; i < cycles; i++) {
     for (let x = coords.x - 1; x <= coords.x + 1; x++) {
       for (let y = coords.y - 1; y <= coords.y + 1; y++) {
         for (let z = coords.z - 1; z <= coords.z + 1; z++) {
-          let item = acc.find((o) => o.x === x && o.y === y && o.z === z)
-          if (!item) {
-            item = { x, y, z, n: 0 }
-            acc.push(item)
+          for (let w = coords.w - 1; w <= coords.w + 1; w++) {
+            let item = acc.find((o) => o.x === x && o.y === y && o.z === z && o.w === w)
+            if (!item) {
+              item = { x, y, z, w, n: 0 }
+              acc.push(item)
+            }
+            if (x === coords.x && y === coords.y && z === coords.z && w === coords.w) item.a = true
+            else item.n++
           }
-          if (x === coords.x && y === coords.y && z === coords.z) item.a = true
-          else item.n++
         }
       }
     }
